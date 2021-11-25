580b6d6a7b3c6280637c00c1b4ce5608b8293901
heisenferg <heisenferg@gmail.com>
2021-11-25 13:23:50 +0100

    1 Abrir botón URl OK

--- app/src/main/java/com/example/intentsgeoemail/MainActivity.java
+++ app/src/main/java/com/example/intentsgeoemail/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.intentsgeoemail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button abrir=findViewById(R.id.buttonWeb);
+        abrir.setOnClickListener(this::abrir);
     }
 
 
-    Intent i = new Intent(Intent.ACTION_SEND);
+    public void abrir(View v){
+        Intent i = new Intent();
+        Intent chooser=null;
+
+        switch(v.getId()){
+            case R.id.buttonWeb:
+                EditText edURL=(EditText)findViewById(R.id.texturl);
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(edURL.getText().toString()));
+                chooser=i.createChooser(i,"Elige Navegador");
+                startActivity(i);
+                Toast.makeText(this.getApplicationContext(),
+                        "Acceso a web!",Toast.LENGTH_LONG).show();                 break;
+
+        }
+    }
 }
